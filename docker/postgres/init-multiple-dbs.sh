c7fc3551910ffcbab7b0e4f62ddb29384832334b
nurashi <nurashi778@gmail.com>
2026-05-02 16:24:44 +0500

    add docker-compose with RabbitMQ and all services

--- docker/postgres/init-multiple-dbs.sh
+++ docker/postgres/init-multiple-dbs.sh
@@ -4,4 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   CREATE DATABASE payment_db;
   CREATE DATABASE order_db;
+  CREATE DATABASE notification_db;
 EOSQL
